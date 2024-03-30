9833f02e71e6502b9aa300ac7d6e3dbad69ceca9
CMRicardo <ricardocorrales84@gmail.com>
2024-03-29 21:53:09 -0600

    feat: :sparkles: Add LoginForm component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+"use client";
+
+import { LoginForm } from "@/components/login-form";
+
 export default function Home() {
   return (
     <>
-      <h1>Hello</h1>
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Litoral Restaurant - Login
+      </h1>
+      <section className="flex flex-col items-center justify-center mt-8">
+        <LoginForm />
+      </section>
     </>
   );
 }
---
+++ components/login-form.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { Button } from "./ui/button";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useToast } from "./ui/use-toast";
+import { ToastAction } from "./ui/toast";
+
+const loginFormSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
+export function LoginForm() {
+  const { toast } = useToast();
+  const loginForm = useForm<z.infer<typeof loginFormSchema>>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const handleSubmit = (data: z.infer<typeof loginFormSchema>) => {
+    toast({
+      title: "Login",
+      description: "Login successful",
+      duration: 2000,
+    });
+  };
+
+  return (
+    <Form {...loginForm}>
+      <form
+        className="w-2/3 space-y-6"
+        onSubmit={loginForm.handleSubmit(handleSubmit)}
+      >
+        <FormField
+          control={loginForm.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="you@email.com" type="email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={loginForm.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="********" type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Login</Button>
+      </form>
+    </Form>
+  );
+}
