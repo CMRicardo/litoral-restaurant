101524b719d3925d759eb32a020488eb3d4dd23e
CMRicardo <ricardo.corrales.montoya@gmail.com>
2024-07-28 21:56:39 -0600

    Add background in light mode

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -15,16 +15,19 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen w-full flex-col bg-muted/40">
-      <CommandBarProvider>
-        <>
-          <Sidebar />
-          <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-            <Header />
-            <Main>{children}</Main>
-          </div>
-        </>
-      </CommandBarProvider>
-    </div>
+    <>
+      <div className="absolute inset-0 -z-10 block h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem] dark:hidden"></div>
+      <div className="flex min-h-screen w-full flex-col bg-muted/40">
+        <CommandBarProvider>
+          <>
+            <Sidebar />
+            <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+              <Header />
+              <Main>{children}</Main>
+            </div>
+          </>
+        </CommandBarProvider>
+      </div>
+    </>
   );
 }
