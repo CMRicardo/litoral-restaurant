db7f330be6ecbb8deb766a6cf5e2f706b13ae5cf
CMRicardo <ricardo.corrales.montoya@gmail.com>
2024-08-25 21:57:48 -0600

    fix: Remove pointer events none and opacity for commandItems to be able to click on them

--- components/ui/command.tsx
+++ components/ui/command.tsx
@@ -117,7 +117,7 @@ const CommandItem = React.forwardRef<
   <CommandPrimitive.Item
     ref={ref}
     className={cn(
-      "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm text-slate-950 outline-none aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 dark:text-slate-300",
+      "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm text-slate-950 outline-none aria-selected:bg-accent aria-selected:text-accent-foreground dark:text-slate-300",
       className,
     )}
     {...props}
