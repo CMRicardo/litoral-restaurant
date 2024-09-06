b099d828f0a112ed30af54e47a39b84d38ecb3b9
CMRicardo <ricardo.corrales.montoya@gmail.com>
2024-09-06 15:39:58 -0600

    Add comment to background

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -16,7 +16,9 @@ export default function DashboardLayout({
 }) {
   return (
     <>
+      {/* Background */}
       <div className="absolute inset-0 -z-10 block h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem] dark:hidden"></div>
+
       <div className="flex min-h-screen w-full flex-col bg-muted/40">
         <CommandBarProvider>
           <>
