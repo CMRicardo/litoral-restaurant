aabce732eefb0a01244a3fa8a52a1080cbe53989
CMRicardo <ricardo.corrales.montoya@gmail.com>
2024-09-06 17:39:19 -0600

    Add new-employee-form

--- app/dashboard/employees/page.tsx
+++ app/dashboard/employees/page.tsx
@@ -30,11 +30,11 @@ import {
   DropdownMenuItem,
 } from "@/components/ui/dropdown-menu";
 import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
-import { ListFilter, PlusCircle, MoreHorizontal, File } from "lucide-react";
+import { ListFilter, MoreHorizontal, File } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
 import Image from "next/image";
 import { Metadata } from "next";
-import Link from "next/link";
+import { NewEmployeeForm } from "@/components/new-employee-form";
 
 export const metadata: Metadata = {
   title: "Employees | Litoral Restaurant",
@@ -80,14 +80,8 @@ export default function Employees() {
                 Export
               </span>
             </Button>
-            <Link href="/dashboard/employees/new">
-              <Button size="sm" className="h-7 gap-1">
-                <PlusCircle className="h-3.5 w-3.5" />
-                <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-                  Add Employee
-                </span>
-              </Button>
-            </Link>
+
+            <NewEmployeeForm />
           </div>
         </div>
         <TabsContent value="all">
--- components/new-employee-form.tsx
+++ components/new-employee-form.tsx
@@ -1,17 +1,15 @@
 "use client";
 
-import Link from "next/link";
-import { ChevronLeft } from "lucide-react";
-
-import { Button } from "@/components/ui/button";
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
   Select,
@@ -20,10 +18,6 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-
-import { Textarea } from "@/components/ui/textarea";
-
-import { ProductImage } from "@/components/product-image";
 import {
   Form,
   FormControl,
@@ -32,24 +26,28 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import { toast } from "sonner";
+
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { toast } from "sonner";
-import { Category } from "@/interfaces/category.interface";
-import { productFormSchema } from "@/schemas/product-form.schema";
+import { PlusCircle, Save, Trash } from "lucide-react";
+
+import { employeeSchema } from "@/schemas/employee-schema";
 
 export const NewEmployeeForm = () => {
-  const form = useForm<z.infer<typeof productFormSchema>>({
-    resolver: zodResolver(productFormSchema),
+  const form = useForm<z.infer<typeof employeeSchema>>({
+    resolver: zodResolver(employeeSchema),
     defaultValues: {
-      name: "",
-      description: "",
-      price: 1,
+      firstName: "",
+      lastName: "",
+      role: 2,
+      email: "",
+      password: "Employee123",
     },
   });
 
-  const onSubmit = (values: z.infer<typeof productFormSchema>) => {
+  const onSubmit = (values: z.infer<typeof employeeSchema>) => {
     toast.success(
       <div>
         <span>Product created successfully!</span>
@@ -57,203 +55,111 @@ export const NewEmployeeForm = () => {
       </div>,
     );
   };
-  const onDiscard = () => {
-    if (!form.formState.isDirty) {
-      toast.error("Product discarded");
-      // TODO: Redirect to the products page
-      return;
-    }
-  };
+
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)}>
-        <div className="flex items-center gap-4 py-4">
-          <Link href="/dashboard/products">
-            <Button
-              type="button"
-              variant="outline"
-              size="icon"
-              className="h-7 w-7"
-            >
-              <ChevronLeft className="h-4 w-4" />
-              <span className="sr-only">Back</span>
-            </Button>
-          </Link>
-          <h1 className="flex-1 shrink-0 whitespace-nowrap text-xl font-semibold tracking-tight sm:grow-0">
-            New Product
-          </h1>
-          <div className="hidden items-center gap-2 md:ml-auto md:flex">
-            <Button
-              onClick={onDiscard}
-              type="button"
-              variant="outline"
-              size="sm"
-            >
-              Discard
-            </Button>
-            <Button type="submit" size="sm">
-              Save Product
-            </Button>
-          </div>
-        </div>
-        <div className="grid gap-4 md:grid-cols-[1fr_250px] lg:grid-cols-3 lg:gap-8">
-          <div className="grid auto-rows-max items-start gap-4 lg:col-span-2 lg:gap-8">
-            <Card>
-              <CardHeader>
-                <CardTitle>Product Details</CardTitle>
-                <CardDescription>Edit details of the product</CardDescription>
-              </CardHeader>
-              <CardContent>
-                <div className="grid gap-6">
-                  <div className="grid gap-3">
-                    <FormField
-                      control={form.control}
-                      name="name"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Name</FormLabel>
-                          <FormControl>
-                            <Input type="text" {...field} />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                  </div>
-                  <div className="grid gap-3">
-                    <FormField
-                      control={form.control}
-                      name="description"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Description</FormLabel>
-                          <FormControl>
-                            <Textarea className="min-h-32" {...field} />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                  </div>
-                </div>
-              </CardContent>
-            </Card>
-            <Card>
-              <CardHeader>
-                <CardTitle>Product Info</CardTitle>
-                <CardDescription>
-                  What category is this product in?
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                <div className="grid gap-6 sm:grid-cols-2">
-                  <div className="grid gap-3">
-                    <FormField
-                      control={form.control}
-                      name="category"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Category</FormLabel>
-                          <FormControl>
-                            <Select
-                              onValueChange={field.onChange}
-                              defaultValue={field.value}
-                            >
-                              <SelectTrigger aria-label="Select category">
-                                <SelectValue
-                                  placeholder={"Select a category"}
-                                />
-                              </SelectTrigger>
-                              <SelectContent>
-                                {/* {categories.map((category) => (
-                                  <SelectItem
-                                    key={category.id}
-                                    value={category.name}
-                                  >
-                                    {category.name}
-                                  </SelectItem>
-                                ))} */}
-                              </SelectContent>
-                            </Select>
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                  </div>
-                  <div className="grid gap-3">
-                    <FormField
-                      control={form.control}
-                      name="status"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>Status</FormLabel>
-                          <FormControl>
-                            <Select
-                              onValueChange={field.onChange}
-                              defaultValue={field.value}
-                            >
-                              <SelectTrigger aria-label="Select status">
-                                <SelectValue placeholder={"Select a status"} />
-                              </SelectTrigger>
-                              <SelectContent>
-                                {/* {POSSIBLE_STATUSES.map((status) => (
-                                  <SelectItem
-                                    key={status}
-                                    value={status}
-                                    className="capitalize"
-                                  >
-                                    {status}
-                                  </SelectItem>
-                                ))} */}
-                              </SelectContent>
-                            </Select>
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                  </div>
-                </div>
-              </CardContent>
-            </Card>
-          </div>
-          <div className="grid auto-rows-max items-start gap-4 lg:gap-8">
-            <Card>
-              <CardHeader>
-                <CardTitle>Price</CardTitle>
-                <CardDescription>Edit the price of the product</CardDescription>
-              </CardHeader>
-              <CardContent>
-                <div className="grid gap-6">
-                  <FormField
-                    control={form.control}
-                    name="price"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Price</FormLabel>
-                        <FormControl>
-                          <Input type="number" {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
-              </CardContent>
-            </Card>
-            <ProductImage />
-          </div>
-        </div>
-        <div className="flex items-center justify-center gap-2 py-4 md:hidden">
-          <Button onClick={onDiscard} type="button" variant="outline" size="sm">
-            Discard
+    <>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button size="sm" className="h-7 gap-1">
+            <PlusCircle className="h-3.5 w-3.5" />
+            <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+              Add Employee
+            </span>
           </Button>
-          <Button type="submit" size="sm">
-            Save Product
-          </Button>
-        </div>
-      </form>
-    </Form>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>New Employee</DialogTitle>
+            <DialogDescription>
+              Make sure all data is correct before saving
+            </DialogDescription>
+          </DialogHeader>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-4">
+              {/* firstName */}
+              <FormField
+                control={form.control}
+                name="firstName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>First Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="John" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {/* lastName */}
+              <FormField
+                control={form.control}
+                name="lastName"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Last Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Doe" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {/* email */}
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Last Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="john@mail.com" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {/* Role */}
+              <FormField
+                control={form.control}
+                name="role"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Role</FormLabel>
+                    <FormControl>
+                      <Select
+                        onValueChange={field.onChange}
+                        defaultValue={String(field.value)}
+                      >
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select a role" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectItem value={"1"}>Admin</SelectItem>
+                          <SelectItem value={"2"}>User</SelectItem>
+                        </SelectContent>
+                      </Select>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex justify-end gap-2">
+                <DialogClose asChild>
+                  <Button variant="destructive" type="button">
+                    <Trash className="mr-2" /> Cancel
+                  </Button>
+                </DialogClose>
+                <DialogClose>
+                  <Button type="submit">
+                    <Save className="mr-2" />
+                    Save
+                  </Button>
+                </DialogClose>
+              </div>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
