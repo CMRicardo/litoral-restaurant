16fd3c277933b07c7b3d1f70930df22d356bf953
CMRicardo <ricardo.corrales.montoya@gmail.com>
2024-09-06 16:18:56 -0600

    Delete new employee page

--- app/dashboard/employees/new/page.tsx
+++
@@ -1,9 +0,0 @@
-import { NewEmployeeForm } from "@/components/new-employee-form";
-
-const NewEmployeePage = () => {
-  <div className="mx-auto grid max-w-[59rem] flex-1 auto-rows-max gap-4">
-    <NewEmployeeForm />
-  </div>;
-};
-
-export default NewEmployeePage;
